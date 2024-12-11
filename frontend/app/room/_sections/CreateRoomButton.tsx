39047c1c1de0b7c08a11993287e9456b091b6dd5
chaeyeon1 <66813821+Chaeyeon1@users.noreply.github.com>
2024-12-12 00:38:13 +0900

    refactor : 코드 정리

--- frontend/app/room/_sections/CreateRoomButton.tsx
+++ frontend/app/room/_sections/CreateRoomButton.tsx
@@ -1,19 +1,6 @@
 'use client';
 import generateUUID from '@/app/(root)/_related/generateUUID';
-import Button from '@/app/_components/Button';
-import {
-  ButtonContainer,
-  CloseButton,
-  ModalBackDrop,
-  ModalContainer,
-  ModalFooter,
-  ModalHeader,
-  ModalHeaderTitle,
-  XIcon,
-} from '@/app/_components/common';
-import Textfield from '@/app/_components/TextField';
 import { useRouter } from 'next/navigation';
-import { useState } from 'react';
 import { CreateRoomButtonContainer } from '../_related/room.styled';
 
 const CreateRoomButton = () => {
